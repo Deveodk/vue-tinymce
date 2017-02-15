3b86d404063f3c6b0e7d78657b6b6263a7969cec
Jason Kelly <jason-kelly@live.dk>
2017-02-15 17:22:46 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import Tinymce from './vue-tinymce.vue'
+const VueTinyMCE = {
+
+    install: (Vue, options) => {
+        Vue.component('vue-tinymce', Tinymce)
+    }
+
+
+}
+if (typeof module === 'object' && module.exports) {
+    module.exports = VueErrorTracker;
+}
